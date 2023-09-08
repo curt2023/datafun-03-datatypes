57c27ede935848d212d3741594ecf808105127ab
Curtis Rogers <ctrogers87@gmail.com>
2023-09-08 18:19:17 -0500

    Passed doc test in xtra_p3.py

--- xtra_p3.py
+++ xtra_p3.py
@@ -1,13 +1,13 @@
 """
 Optional bonus. See course site for details.
 
-len(longwordset1)
+>>> len(longwordset1)
 415
 
-len(longwordset2)
+>>> len(longwordset2)
 197
 
-len(longwords)
+>>> len(longwords)
 13
 """
 
@@ -59,19 +59,23 @@ def compare_two_plays():
     # intersection of the two sets
     longwords = longwordset1 & longwordset2
 
-    
-
     # print the length of the sets and the list
     print(len(longwordset1))
     print(len(longwordset2))
     print(len(longwords))
     print()
-    print(f"{sorted(longwords) = }")
+    print(sorted(longwords))
     print()
 
-    print("TESTING...if nothing prints before the testing is done, you passed!")
+    logger.info(f"{len(longwordset1) = }")
+    logger.info(f"{len(longwordset2) = }")
+    logger.info(f"{len(longwords) = }")
+    logger.info("=====================================================")
+    logger.info("=====================================================")
+    logger.info("doctest.testmod results")
+    logger.info("TESTING...if nothing prints before the testing is done, you passed!")
     doctest.testmod()
-    print("TESTING DONE")
+    logger.info("TESTING DONE")
     
 
 def show_log():
@@ -84,19 +88,41 @@ def show_log():
 # Call some functions and execute code!
 
 if __name__ == "__main__":
+    def compare():
+        logger.inf("Compare the text lists")
+        
+    with open("text_hamlet.txt", "r") as f1:
+        text = f1.read()
+        wordlist1 = text.split()  # split on whitespace
+
+    #logger.info(f"List of words from play 1: {wordlist1}")
+
+    with open("text_juliuscaesar.txt", "r") as f2:
+        text = f2.read()
+        wordlist2 = text.split()  
+
+    
+    wordset1 = set(wordlist1) 
+    wordset2 = set(wordlist2)  
+
+    maxlen = 10  
+
+    longwordlist1 = [word for word in wordset1 if len(word) > maxlen]
+    longwordlist2 = [word for word in wordset2 if len(word) > maxlen]
+
+    longwordset1 = set(longwordlist1)  
+    longwordset2 = set(longwordlist2)
+
+    longwords = longwordset1 & longwordset2
+
+
+
     logger.info("Calling functions from main block")
 
     compare_two_plays()
-   
-    
-    
 
-    
 
     logger.info("Complete the code to compare two plays.")
     show_log()
 
-    print("TESTING...if nothing prints before the testing is done, you passed!")
-    doctest.testmod()
-    print("TESTING DONE")
 
